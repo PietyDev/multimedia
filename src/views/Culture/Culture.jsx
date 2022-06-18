ab62efb187ec2f0fd20903ca0b117e0f2e1475a0
sito8943 <sito8943@gmail.com>
2022-06-18 18:44:16 -0400

    other views

---
+++ src/views/Culture/Culture.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/function-component-definition */
+
+import { useEffect } from "react";
+
+// contexts
+import { useLanguage } from "context/LanguageProvider";
+
+// @mui components
+import { useTheme, Button, Link, Typography } from "@mui/material";
+
+// own components
+import Container from "components/Container/Container";
+
+// layouts
+import Hero from "layouts/Hero/Hero";
+
+const Culture = () => {
+  const { languageState } = useLanguage();
+  const theme = useTheme();
+
+  useEffect(() => {}, []);
+  return (
+    <Container>
+      <Hero>
+        <Typography variant="h1">{languageState.texts.Culture.Title}</Typography>
+        <Container sx={{ marginTop: "20vh" }}>
+          {languageState.texts.Links.map(
+            (item, index) =>
+              index !== 0 && (
+                <Link key={item.to} to={item.to} style={{ textDecoration: "none" }}>
+                  <Button
+                    variant="outlined"
+                    color="primary"
+                    sx={{
+                      textTransform: "capitalize",
+                      marginRight: "20px",
+                      borderRadius: "1rem",
+                      "&:hover": {
+                        background: theme.palette.primary.main,
+                        color: "aliceblue",
+                      },
+                    }}
+                  >
+                    {item.label}
+                  </Button>
+                </Link>
+              )
+          )}
+        </Container>
+      </Hero>
+    </Container>
+  );
+};
+
+export default Culture;
