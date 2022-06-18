1e15852029522eed313ee8c3594cc923a1f02564
sito8943 <sito8943@gmail.com>
2022-06-18 15:29:56 -0400

    react init

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+// own components
+import Container from "../Container/Container";
+
+const Navbar = () => {
+  return <Container>Hola</Container>;
+};
+
+export default Navbar;
