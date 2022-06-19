895984c048cf53f9bef32ce62d2631c98fa7b1f6
sito8943 <sito8943@gmail.com>
2022-06-18 21:10:08 -0400

    Home sections

---
+++ src/views/Home/Sections/Gallery/Gallery.jsx
@@ -0,0 +1,111 @@
+/* eslint-disable react/function-component-definition */
+
+// framer-motion
+import { motion } from "framer-motion";
+
+// contexts
+import { useLanguage } from "context/LanguageProvider";
+
+// @mui components
+import { Typography } from "@mui/material";
+
+// own components
+import Container from "components/Container/Container";
+import Image from "components/Image/Image";
+
+// layouts
+import Section from "layouts/Section/Section";
+
+// images
+import heroBackground from "assets/images/color.webp";
+
+const Gallery = () => {
+  const { languageState } = useLanguage();
+
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const ulItem = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  const apparition = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  return (
+    <Section>
+      <motion.div
+        variants={apparition}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+      >
+        <Typography variant="h3">{languageState.texts.Home.Sections.Gallery.Title}</Typography>
+      </motion.div>
+      <motion.div
+        variants={apparition}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+      >
+        <Typography variant="body1" sx={{ marginLeft: "3px", marginTop: "10px" }}>
+          {languageState.texts.Home.Sections.Gallery.Description}
+        </Typography>
+      </motion.div>
+      <motion.ul
+        variants={container}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        style={{ display: "flex", padding: 0, flexWrap: "wrap" }}
+      >
+        {[
+          { img: heroBackground, id: 0 },
+          { img: heroBackground, id: 1 },
+          { img: heroBackground, id: 2 },
+          { img: heroBackground, id: 3 },
+          { img: heroBackground, id: 4 },
+          { img: heroBackground, id: 5 },
+          { img: heroBackground, id: 6 },
+        ].map((item) => (
+          <motion.div key={item.to} variants={ulItem} viewport={{ once: true }}>
+            <Container
+              key={item.id}
+              sx={{
+                img: {
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "cover",
+                  borderRadius: "1rem",
+                  margin: "10px",
+                },
+              }}
+            >
+              <Image img={item.img} alt={item.id} />
+            </Container>
+          </motion.div>
+        ))}
+      </motion.ul>
+    </Section>
+  );
+};
+
+export default Gallery;
