28c713123a7665c2b8f1b42481e3d3abe00cc757
Sito8943 <sito8943@gmail.com>
2022-06-27 09:24:16 -0400

    news

--- src/views/Home/Sections/News/News.jsx
+++ src/views/Home/Sections/News/News.jsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 import { useLanguage } from "context/LanguageProvider";
 
 // @mui components
-import { Grid, Typography } from "@mui/material";
+import { Grid, Link, Typography } from "@mui/material";
 
 // own components
 import Container from "components/Container/Container";
@@ -18,6 +18,9 @@ import Section from "layouts/Section/Section";
 
 // images
 import cobre012 from "assets/images/cobre012.jpg";
+import diaz from "assets/images/diaz.jpg";
+import truck from "assets/images/truck.jpg";
+import hotel from "assets/images/hotel.jpg";
 
 const News = () => {
   const { languageState } = useLanguage();
@@ -53,6 +56,27 @@ const News = () => {
   const news = [
     {
       id: 0,
+      url: "http://www.cubadebate.cu/noticias/2019/07/24/proyecto-cubano-italiano-mejorara-acceso-al-poblado-del-cobre/",
+      title: "Proyecto cubano-italiano mejorará acceso al poblado del Cobre",
+      date: "24 julio 2019 ",
+      photo: truck,
+    },
+    {
+      id: 1,
+      title: "Dos nuevos hoteles para El Cobre, Santiago de Cuba",
+      date: "5 octubre 2018",
+      url: "http://www.cubadebate.cu/noticias/2018/10/05/dos-nuevos-hoteles-para-el-cobre-santiago-de-cuba/",
+      photo: hotel,
+    },
+    {
+      id: 2,
+      url: "http://www.cubadebate.cu/noticias/2018/06/20/diaz-canel-inicia-visita-a-santiago-de-cuba/",
+      date: "20 junio 2018",
+      title: "Díaz-Canel en Santiago de Cuba: Viaje a la semilla de la Revolución (+ Video)",
+      photo: diaz,
+    },
+    {
+      id: 3,
       url: "http://www.cubadebate.cu/noticias/2017/11/11/revitalizan-emblematico-poblado-cubano-de-el-cobre/",
       photo: cobre012,
       title: "Revitalizan emblemático poblado cubano de El Cobre",
@@ -96,22 +120,27 @@ const News = () => {
           {news.map((item) => (
             <Grid item key={item.id}>
               <motion.div variants={ulItem} viewport={{ once: true }}>
-                <Container
-                  key={item.id}
-                  sx={{
-                    img: {
+                <Link href={item.url} target="_blank" rel="noopener">
+                  <Container
+                    flexDirection="column"
+                    key={item.id}
+                    sx={{
                       width: "350px",
-                      height: "200px",
-                      objectFit: "cover",
-                      borderRadius: "1rem",
-                      margin: "10px",
-                    },
-                  }}
-                >
-                  <Image img={item.photo} alt={item.title} />
-                  <Typography variant="subtitle1">{item.title}</Typography>
-                  <Typography variant="subtitle2">{item.date}</Typography>
-                </Container>
+                      marginRight: "20px",
+                      img: {
+                        width: "100%",
+                        height: "200px",
+                        objectFit: "cover",
+                        borderRadius: "1rem",
+                        margin: "10px",
+                      },
+                    }}
+                  >
+                    <Image img={item.photo} alt={item.title} />
+                    <Typography variant="subtitle1">{item.title}</Typography>
+                    <Typography variant="subtitle2">{item.date}</Typography>
+                  </Container>
+                </Link>
               </motion.div>
             </Grid>
           ))}
